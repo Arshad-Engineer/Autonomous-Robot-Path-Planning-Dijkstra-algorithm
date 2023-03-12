34e78a7bfbca8bfe810a153bfd6ae4bef41401bb
Arshad-Engineer <112987383+Arshad-Engineer@users.noreply.github.com>
2023-03-11 20:03:20 -0500

    Class implementation of Dijkstra algorithm

--- dijkstra_arshad_shaik.py
+++ dijkstra_arshad_shaik.py
@@ -1,41 +1,80 @@
 # %%
 import heapq as hq
 
+class Dijkstra:
+    # Class variable
+    # pi = 3.14
+    # Initializing the list to be used in priority queue
+    Q = []
 
-# updatinga a node in heapq
+    # Circle gets instantiated with a radius (default is 1)
+    def __init__(self, start_node, goal_node):
+        self.start_node = start_node
+        self.goal_node = goal_node
 
-# creeating a tuple with cost to come, node index, parent node index = 0, and coordinate values (x,y)
+    # Initializing a node list with start node
+    def initializeNodeList(self):
+        # creating a tuple with cost to come, node index, parent node index = 0, and coordinate values (x,y)
+        Dijkstra.Q.append(self.start_node)
+        print('Initialized node list: ', Dijkstra.Q)
 
-d1 = (34, 1, 0, (2,3))
+        return Dijkstra.Q
+    
+    def pushNode2List(self, Q, new_node):
+        # using heapify to convert list into heap
+        hq.heapify(Q)
+        # using heappush() to push elements into heap
+        hq.heappush(Q, new_node)
+        print("List, after pushing new node to the queue ", Q)
+        
+        return list(Q)    
 
-d2 = (43, 2, 0, (4,5))
+    # Method for updatig a node in list via heapq
+    def updateNodeList(self, Q, node_idx, new_node):
+        # using heapify to convert list into heap
+        hq.heapify(Q)
 
-d3 = (23, 3, 0, (7,1))
+        # creating a tuple with cost to come, node index, parent node index = 0, and coordinate values (x,y)
+        x, y = node_idx
 
-# Initializing the list to be used in priority queue
-Q = []
-print('Initialized node list: ', Q)
+        # Updating a node in the list
+        for i in range(len(Q)):
+            if Q[i][3] == (x,y):
+                Q[i] = new_node
+        
+        print("List, after updating node ",Q)
 
-# Push elements to the queue
+        return list(Q)
 
-hq.heappush(Q, d1)
-hq.heappush(Q, d2)
-hq.heappush(Q, d3)
+    
+    def popMinNodefrmList(self, Q):
+        # using heapify to convert list into heap
+        hq.heapify(Q)
+        # Push elements to the queue
+        min_node = hq.heappop(Q)
+        print("Minimum Node in the  list: ", min_node)
+        print("Updated List: ", list(Q))
 
-print("List, after pushing elements to the queue ", Q)
-
-hq.heapify(Q)
-print("List, after heapify: ", Q)
-
-# Updating a node in the list
-for i in range(3):
-    if Q[i][3] == (4,5):
-        Q[i] = (20, 1, 0, (4,5))
-
-hq.heapify(Q)
-print("List, after modifying & heapify ",Q)
+        return min_node, list(Q)
+# %%
+# initialize node
+c = Dijkstra((0, 0, 0, (6,6)), (100, 100, 0, (200,600)))
+nodesQueue = c.initializeNodeList()
 
 # %%
+# push a node to the list
+new_node1 = (10, 1, 0, (10,10))
+nodesQueue = c.pushNode2List(nodesQueue, new_node1)
 
+# %%
+# push another node to the list
+new_node2 = (50, 2, 0, (20,20))
+nodesQueue = c.pushNode2List(nodesQueue, new_node2)
 
+# %%
+#  update a particular node in the e list
+updated_node = (0, 3, 0, (6,6)) # new node1 is modified on index
+nodesQueue = c.updateNodeList(nodesQueue, (6,6), updated_node)
+minimum_node, nodesQueue = c.popMinNodefrmList(nodesQueue)
 
+# %%
